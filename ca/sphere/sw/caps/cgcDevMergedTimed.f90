1421a2f84d5a82886d8b37e854729f973a4d5ce6
AHolmes_zbook <s1952531@ed.ac.uk>
2025-11-19 16:21:07 +0000

    added merged timed version

---
+++ ca/sphere/sw/caps/cgcDevMergedTimed.f90
@@ -0,0 +1,530 @@
+program cgcDev
+    implicit none
+
+    !constants for the default test case
+    !twopi, dlfi, pi, dq, ngf, ntf, hpi, clonf, slonf, f12, f14
+    double precision,parameter:: one=1.d0, two=2.d0, four=4.d0
+    double precision,parameter:: pi=3.141592653589793238462643383279502884197169399375105820974944592307816d0
+    double precision,parameter:: hpi=pi/two, twopi=two*pi
+    double precision,parameter:: f12=one/two, f14=one/four
+    double precision,parameter:: small=1.d-12
+    double precision,parameter:: fpole=two*6.2831853071 !set in flow-setup as fpole=twopi usin shorter pi and 10 dps (see line 191)
+    integer,parameter:: ncont=80 !default as 80 in line 430 of flow-setup
+    double precision,parameter:: dq=two*fpole/dble(ncont)
+    integer,parameter:: ng=128, nt=2*ng !ng set in line 105 of flow-setup
+    integer,parameter:: mgf=4, ngf=ng*mgf, ntf=nt*mgf
+    double precision:: clonf(ntf),slonf(ntf)
+    integer:: i, j, iterCount
+    double precision:: rlonf
+    double precision:: dlf,dlfi
+    integer,parameter:: ngridp=ng*nt
+    integer,parameter:: npm=200*ngridp
+    double precision,parameter:: dl=twopi/dble(nt)
+    !end of vars/constants outside of contours
+
+    !defined in contours
+    double precision:: x(npm),y(npm),z(npm)
+    integer:: next(0:npm),npt
+    integer(kind=8):: callCount !as used in calc of pos for large file
+    double precision:: fcor(ng)
+
+    integer:: numInputs
+    integer, parameter:: numIters=1 !238
+    integer:: totReads
+
+    double precision:: qc(ng,nt)
+    double precision, allocatable:: qcDiffs(:) !of shape (totReads)
+                                               !according to gprof there are 23810 calls for the default test case
+
+    double precision, allocatable:: x_arr(:, :) !of shape (npm, numInputs)
+    double precision, allocatable:: y_arr(:, :) !of shape (npm, numInputs)
+    double precision, allocatable:: z_arr(:, :) !of shape (npm, numInputs)
+    integer, allocatable:: next_arr(:, :) !of shape (0:npm, numInputs)
+    integer, allocatable:: npt_arr(:) !of shape (numInputs)
+
+    double precision, allocatable:: qc_arr(:,:,:) !of shape (ng,nt, numInputs)
+
+    integer:: qcSize=size(qc) * storage_size(qc)/8 !div by 8 to get bytes
+    integer(kind=8):: input_block_size= storage_size(x)/8 * size(x) + &
+                                storage_size(y)/8 * size(y) + &
+                                storage_size(z)/8 * size(z) + &
+                                storage_size(next)/8 * size(next) + &
+                                storage_size(npt)/8
+
+
+    !Timers
+    double precision:: con2gridToTTime=0.0d0, preAvgTotTime=0.0d0, avgTotTime=0.0d0
+    double precision:: l1TotTime=0.0d0, l23TotTime=0.0d0, l4TotTime=0.0d0, l5TotTime=0.0d0
+    double precision:: l6TotTime=0.0d0
+
+    call init
+    
+    !main loop
+    do iterCount=1,numIters
+        do callCount = 1, numInputs
+            x = x_arr(:, callCount)
+            y = y_arr(:, callCount)
+            z = z_arr(:, callCount)
+            next = next_arr(:, callCount)
+            npt = npt_arr(callCount)
+            call con2grid(qc)
+            call compare_qcs
+        end do
+    end do
+
+    !print the max of the qcDiffs
+    print *, 'Max difference in qc: ', maxval(qcDiffs)
+
+    call finish
+
+    contains
+
+    subroutine init
+        call openFiles
+        call initVars
+        
+        do callCount = 1, numInputs
+            call readInput
+            !call readInputReversed
+            call readOutputs
+            !call readOutputsReversed
+        end do
+        call closeFiles
+    end subroutine
+
+
+    subroutine initVars  
+        dlf =twopi/dble(ntf)
+
+        do i=1,ntf
+            rlonf=dlf*dble(i-1)-pi
+            clonf(i)=cos(rlonf)
+            slonf(i)=sin(rlonf)
+        enddo
+
+        do j=1,ng
+            fcor(j)=fpole*sin((dble(j)-f12)*dl-hpi)
+        enddo
+
+        dlfi=dble(ntf)/(twopi+small)
+
+        call getNumWrites
+
+        totReads=numInputs*numIters
+
+        call allocateVars
+
+        return
+    end subroutine
+
+    subroutine allocateVars
+        allocate(x_arr(npm, numInputs))
+        allocate(y_arr(npm, numInputs))
+        allocate(z_arr(npm, numInputs))
+        allocate(next_arr(0:npm, numInputs))
+        allocate(npt_arr(numInputs))
+        allocate(qc_arr(ng,nt, numInputs))
+        allocate(qcDiffs(totReads))
+        return
+    end subroutine
+
+    subroutine getNumWrites
+        !determine how many writes are in cgc_* files
+        !inputs and outputs written same number of times, easier to calc size of cgc_outputs.dat since only qc written
+        integer :: filesize
+        
+        inquire(unit=102, size=filesize)
+        !print *, 'File size of cgc_outputs.dat: ', filesize
+        !print *, 'Size of one qc array: ', qcSize
+        numInputs=filesize/qcSize
+        !print *, 'Number of inputs/outputs in files: ', numInputs
+
+        return
+    end subroutine
+
+
+    subroutine openFiles
+        open(100, file="cgc_inputs.dat", status='old', action='read', access='stream', form='unformatted')
+        open(102, file="cgc_outputs.dat", status='old', action='read', access='stream', form='unformatted')
+        return
+    end subroutine
+
+    subroutine closeFiles
+        close(100)
+        close(102)
+        return
+    end subroutine
+
+
+    subroutine readInput
+        ! Reads in the contour data from a file "cgc_inputs.dat"
+
+        integer(kind=8) :: current_pos
+        inquire(unit=100, pos=current_pos)
+        !print *, 'callCount: ', callCount
+        !print *, 'Reading input at position: ', current_pos
+
+        read(100) x_arr(:, callCount)
+        read(100) y_arr(:, callCount)
+        read(100) z_arr(:, callCount)
+        read(100) next_arr(:, callCount)
+        read(100) npt_arr(callCount)
+        return
+    end subroutine
+
+    subroutine readInputReversed
+        !Reads in the contour data from a file "cgc_inputs.dat" in reverse order
+        !to test work balance
+
+        integer(kind=8):: filesize !kind=8 to hold large file sizes
+        integer(kind=8):: pos
+        
+        inquire(unit=100, size=filesize)
+
+        !print *, 'File size of cgc_inputs.dat: ', filesize
+        !print *, 'Input block size: ', input_block_size
+        !print *, 'Call count: ', callCount
+
+        pos = 1_8 + filesize - int(input_block_size*callCount, kind=8)
+
+        !print *, 'Reading input at position: ', pos
+
+        read(100, pos=pos) x_arr(:, callCount)
+        read(100) y_arr(:, callCount)
+        read(100) z_arr(:, callCount)
+        read(100) next_arr(:, callCount)
+        read(100) npt_arr(callCount)
+
+    end subroutine
+
+    subroutine con2grid(qc)
+        ! Calculates the PV anomaly field (stored in qc) from the PV 
+        ! contours (x,y,z).  Takes away Coriolis frequency (fcor).
+
+        implicit double precision(a-h,o-z)
+        implicit integer(i-n)
+
+        !Passed arrays:
+        double precision:: qc(ng,nt)
+        !Local arrays:
+        double precision:: qa(0:ngf+1,ntf)
+        double precision:: qaend(ngf/2)
+        integer:: ilm1(npt),ntc(npt)
+        double precision:: cx(npt),cy(npt),cz(npt)
+        double precision:: sq(npt)
+
+        !timers (overall and one for each loop)
+        double precision:: startTime, endTime, totalTime
+        double precision:: l1Start, l1End, l1Time
+        double precision:: l23Start, l23End, l23Time
+        double precision:: l4Start, l4End, l4Time
+        double precision:: l5Start, l5End, l5Time
+        double precision:: l6Start, l6End, l6Time
+
+        double precision:: preAvgStart, preAvgEnd, preAvgTime
+        double precision:: avgStart, avgEnd, avgTime
+
+        ! %      cumulative self     calls    
+        ! 7.32    763.92    84.85    23810  __contours_MOD_con2grid
+        ! 1.83   1067.81    21.25    23810  __contours_MOD_con2grid_avg
+
+        startTime = omp_get_wtime()
+        preAvgStart = startTime
+
+        !Initialise crossing information:
+
+        !!$OMP PARALLEL DEFAULT(NONE) SHARED(npt,dlfi,x,y,z,next,ilm1,cx,cy,cz,ntc,sq, zero) PRIVATE(k,ka,sig)
+            !!$OMP DO SCHEDULE(STATIC)
+                !LOOP 1
+                l1Start = omp_get_wtime()
+                !$OMP PARALLEL DO SCHEDULE(GUIDED)
+                do k=1,npt
+                    ilm1(k)=int(dlfi*(pi+atan2(y(k),x(k))))
+                enddo
+                !$OMP END PARALLEL DO
+                l1End = omp_get_wtime()
+                l1Time = l1End - l1Start
+            !!$OMP END DO
+
+            !!$OMP DO SCHEDULE(STATIC)
+                !LOOP 2
+                l23Start = omp_get_wtime()
+                do k=1,npt
+                    ka=next(k)
+                    cx(k)=z(k)*y(ka)-y(k)*z(ka)
+                    cy(k)=x(k)*z(ka)-z(k)*x(ka)
+                    cz(k)=x(k)*y(ka)-y(k)*x(ka)
+                    ntc(k)=ilm1(ka)-ilm1(k)
+              
+                    sig=sign(one,cz(k))
+                    sq(k)=dq*sig
+                    ntc(k)=ntc(k)-ntf*((2*ntc(k))/ntf)
+                    if (sig*dble(ntc(k)) .lt. zero) ntc(k)=-ntc(k)
+                        if (abs(cz(k)) .gt. zero) then
+                            cx(k)=cx(k)/cz(k)
+                            cy(k)=cy(k)/cz(k)
+                        endif
+                enddo
+                l23End = omp_get_wtime()
+                l23Time = l23End - l23Start
+            !!$OMP END DO
+        !!$OMP END PARALLEL
+
+        !----------------------------------------------------------------------
+        !Initialise PV jump array:
+        !LOOP 4
+        l4Start = omp_get_wtime()        
+        do i=1,ntf
+            do j=0,ngf+1
+                qa(j,i)=zero
+            enddo
+        enddo
+        l4End = omp_get_wtime()
+        l4Time = l4End - l4Start
+
+        !Determine crossing indices:
+        !LOOP 5
+        l5Start = omp_get_wtime()
+        !!$OMP PARALLEL DO SCHEDULE(GUIDED)
+        do k=1,npt
+            if (ntc(k) .ne. 0) then
+                jump=sign(1,ntc(k))
+                ioff=ntf+ilm1(k)+(1+jump)/2
+                ncr=0
+                do while (ncr .ne. ntc(k))
+                    i=1+mod(ioff+ncr,ntf)
+                    rlatc=dlfi*(hpi+atan(cx(k)*clonf(i)+cy(k)*slonf(i)))
+                    j=int(rlatc)+1
+                    p=rlatc-dble(j-1)
+                    !!$OMP ATOMIC
+                    qa(j,i)=  qa(j,i)+(one-p)*sq(k)
+                    !!$OMP ATOMIC
+                    qa(j+1,i)=qa(j+1,i)+    p*sq(k)
+                    ncr=ncr+jump    
+                enddo
+            endif
+        enddo
+        !!$OMP END PARALLEL DO
+        l5End = omp_get_wtime()
+        l5Time = l5End - l5Start
+
+        !Get PV values, at half latitudes, by sweeping through latitudes:
+        !LOOP 6
+        l6Start = omp_get_wtime()
+        do i=1,ntf
+            do j=2,ngf
+                qa(j,i)=qa(j,i)+qa(j-1,i)
+            enddo
+        enddo
+        l6End = omp_get_wtime()
+        l6Time = l6End - l6Start
+        !Here, qa(j,i) stands for the PV at latitude j-1/2,
+        !from j = 1, ..., ngf.
+
+        preAvgEnd = omp_get_wtime()
+        preAvgTime = preAvgEnd - preAvgStart
+    
+        !----------------------------------------------------------------------
+        ! %      cumulative self     calls    
+        ! 7.32    763.92    84.85    23810  __contours_MOD_con2grid
+        ! 1.83   1067.81    21.25    23810  __contours_MOD_con2grid_avg
+
+        avgStart = omp_get_wtime()
+
+        !Average PV values on the fine grid to get corresponding 
+        !values on the inversion grid (ng,nt):
+        ngh=ngf
+        nth=ntf
+        
+        do while (ngh .gt. ng) !need to precalc number of iters if want to parallelise with OMP
+            !Pre-store PV adjacent to poles at complementary longitudes (+pi):
+            nthh=nth/2
+            nghp1=ngh+1
+            do i=1,nthh
+                ic=i+nthh
+                qa(0,i)=qa(1,ic)
+                qa(0,ic)=qa(1,i)
+                qa(nghp1,i)=qa(ngh,ic)
+                qa(nghp1,ic)=qa(ngh,i)
+            enddo
+
+            !Work from SP to NP to define PV at full latitudes from averages
+            !at adjacent half latitudes:
+            do i=1,nth
+                do j=0,ngh
+                qa(j,i)=f12*(qa(j+1,i)+qa(j,i))
+                enddo
+            enddo
+
+            !Now qa(j,i) is the PV at latitude j*(pi/ngh)-pi/2
+
+            !Next 1-2-1 average these values to define PV at half latitudes
+            !on a grid twice as coarse:
+            nghh=ngh/2
+            do i=1,nth
+                do j=1,nghh
+                je=2*j
+                qa(j,i)=f12*qa(je-1,i)+f14*(qa(je-2,i)+qa(je,i))
+                enddo
+            enddo
+
+            !Now perform analogous longitudinal 1-2-1 average:
+            do j=1,nghh
+                qaend(j)=f12*(qa(j,nth)+qa(j,1))
+            enddo
+
+            do i=1,nth-1
+                ip1=i+1
+                do j=1,nghh
+                qa(j,i)=f12*(qa(j,i)+qa(j,ip1))
+                enddo
+            enddo
+
+            do j=1,nghh
+                qa(j,nth)=qaend(j)
+            enddo
+            !Now qa(j,i) gives the PV at the half-longitudes i + 1/2.
+
+            !Average these on the twice coarser grid:
+            nthh=nth/2
+
+            do j=1,nghh
+                qa(j,1)=f12*(qa(j,nth)+qa(j,1))
+            enddo
+
+            do i=2,nthh
+                io=2*i-1
+                ie=io-1
+                do j=1,nghh
+                qa(j,i)=f12*(qa(j,ie)+qa(j,io))
+                enddo
+            enddo
+
+            ngh=nghh
+            nth=nthh
+
+        enddo
+
+        !Finalise and take away f to define PV anomaly:
+        do i=1,nt
+            do j=1,ng
+                qc(j,i)=qa(j,i)-fcor(j)
+            enddo
+        enddo
+
+        avgEnd = omp_get_wtime()
+        avgTime = avgEnd - avgStart
+
+        endTime = omp_get_wtime()
+        totalTime = endTime - startTime
+        !print *, 'call', callCount, ' of con2grid took ', totalTime, ' seconds.'
+
+        call accumulateTimes(totalTime, preAvgTime, avgTime, &
+                              l1Time, l23Time, l4Time, l5Time, &
+                              l6Time)
+        
+        return
+        
+    end subroutine 
+
+    subroutine accumulateTimes(totalTime, preAvgTime, avgTime, &
+                              l1Time, l23Time, l4Time, l5Time, &
+                              l6Time)
+
+        !passed args
+        double precision:: totalTime, preAvgTime, avgTime
+        double precision:: l1Time, l23Time, l4Time, l5Time
+        double precision:: l6Time
+
+        !accumulate times into total timers
+        con2gridToTTime = con2gridToTTime + totalTime
+        preAvgTotTime = preAvgTotTime + preAvgTime
+        avgTotTime = avgTotTime + avgTime
+        l1TotTime = l1TotTime + l1Time
+        l23TotTime = l23TotTime + l23Time
+        l4TotTime = l4TotTime + l4Time
+        l5TotTime = l5TotTime + l5Time
+        l6TotTime = l6TotTime + l6Time
+        return
+    end subroutine
+
+    subroutine readOutputs
+
+        integer:: current_pos
+        inquire(unit=102, pos=current_pos)
+        !print *, 'callCount: ', callCount
+        !print *, 'Reading output at position: ', current_pos
+
+        read(102) qc_arr(:,:, callCount)
+        return
+    end subroutine
+
+    subroutine readOutputsReversed
+        integer:: filesize
+        integer:: pos
+        
+        inquire(unit=102, size=filesize)
+        pos = 1 + filesize - qcSize*callCount
+
+        !print *, 'File size of cgc_outputs.dat: ', filesize
+        !print *, 'QC block size: ', qcSize
+        !print *, 'Call count: ', callCount
+
+        read(102, pos=pos) qc_arr(:,:, callCount)
+
+        return
+    end subroutine
+
+    subroutine compare_qcs
+        ! ensure that qc computed matches qc from file
+        implicit double precision(a-h,o-z)
+        implicit integer(i-n)
+        double precision:: qc_file(ng,nt)
+        double precision:: max_diff
+        integer:: i,j
+        
+        qc_file = qc_arr(:,:, callCount)
+        
+        max_diff = 0.0d0
+        do j=1,nt
+            do i=1,ng
+                if (abs(qc(i,j) - qc_file(i,j)) > max_diff) then
+                    max_diff = abs(qc(i,j) - qc_file(i,j))
+                endif
+            enddo
+        enddo
+
+        qcDiffs((iterCount-1)*numInputs + callCount) = max_diff
+
+        return
+    end subroutine
+
+    subroutine finish
+        call printTimes
+        call deallocateVars
+        return
+    end subroutine
+
+    subroutine printTimes
+        print *, 'Total con2grid time: ', con2gridToTTime
+        print *, 'preAvg time', preAvgTotTime
+        print *, 'Avg time', avgTotTime
+        print *, 'Loop 1 total time: ', l1TotTime
+        print *, 'Loop 2/3 total time: ', l23TotTime
+        print *, 'Loop 4 total time: ', l4TotTime
+        print *, 'Loop 5 total time: ', l5TotTime
+        print *, 'Loop 6 total time: ', l6TotTime
+        return
+    end subroutine
+
+    subroutine deallocateVars
+        deallocate(x_arr)
+        deallocate(y_arr)
+        deallocate(z_arr)
+        deallocate(next_arr)
+        deallocate(npt_arr)
+        deallocate(qc_arr)
+        return
+    end subroutine
+
+end program cgcDev
