8ad701c182bf4b5e5dc0715f52d324a902fd7865
AHolmes_zbook <s1952531@ed.ac.uk>
2025-11-25 14:38:24 +0000

    write k and accompanying i,j to file for Loop 5

--- ca/sphere/sw/caps/cgcDev.f90
+++ ca/sphere/sw/caps/cgcDev.f90
@@ -129,12 +129,14 @@ program cgcDev
 
 
     subroutine openFiles
+        open(1, file="loop5_k_ij.txt", action='write', status='replace')
         open(100, file="cgc_inputs.dat", status='old', action='read', access='stream', form='unformatted')
         open(102, file="cgc_outputs.dat", status='old', action='read', access='stream', form='unformatted')
         return
     end subroutine
 
     subroutine closeFiles
+        close(1)
         close(100)
         close(102)
         return
@@ -223,6 +225,8 @@ program cgcDev
                 qa(j,i)=  qa(j,i)+(one-p)*sq(k)
                 qa(j+1,i)=qa(j+1,i)+    p*sq(k)
                 ncr=ncr+jump
+
+                write(1,*) 'k=',k,' i=',i,' j=',j
                 enddo
             endif
         enddo
