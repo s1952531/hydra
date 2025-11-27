ba64beebe4c3adf68de69ef365efe0121b25c0c9
AHolmes_zbook <s1952531@ed.ac.uk>
2025-11-27 11:05:41 +0000

    formatted write requires (150, *)

--- ca/sphere/sw/caps/contoursDev.f90
+++ ca/sphere/sw/caps/contoursDev.f90
@@ -1164,9 +1164,9 @@ subroutine write_corners()
   integer:: i
   cornerWriteCount = cornerWriteCount + 1
   open(150, file="corners.dat", status='unknown', position='append', action='write', form='formatted')
-  write(150) cornerWriteCount
+  write(150, *) cornerWriteCount
   do i=1, size(corners)
-    write(150) corners(i)
+    write(150, *) corners(i)
   enddo
   close(150)
 end subroutine
