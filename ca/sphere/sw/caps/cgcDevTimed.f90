c4c7a2a3f2014a8b6a459cf1f557322f4b60f45e
AHolmes_zbook <s1952531@ed.ac.uk>
2025-12-15 15:16:39 +0000

    added more debug prints to check init

--- ca/sphere/sw/caps/cgcDevTimed.f90
+++ ca/sphere/sw/caps/cgcDevTimed.f90
@@ -966,6 +966,10 @@ program cgcDev
                     enddo
                 enddo
             !$omp end do
+
+            !$omp single
+                print *, 'Initialized qa and qa_jp1 for repeat ks...'
+            !$omp end single
             
             !$omp do schedule(static, 1)
                 do groupCount = 1, nNonRepeatKgroups
@@ -982,6 +986,14 @@ program cgcDev
                     enddo
                 enddo
             !$omp end do
+
+            !$omp single
+                print *, 'Initialized qa and qa_jp1 for nonRepeat ks...'
+            !$omp end single
+
+            !$omp single
+                print *, 'Initialized qa and qa_jp1 for nonAccessed ks...'
+            !$omp end single
 
             !$omp do schedule(dynamic, 1) 
             !in Loop 5 these are not accessed so don't need to first touch them to a specific thread
