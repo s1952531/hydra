7e040a96746eff4aa2f51961252a8fec310d48da
AHolmes_zbook <s1952531@ed.ac.uk>
2026-04-14 12:39:21 +0100

    removing constants from shared (think I ctrl Z'd too many times so possibly redoing what was in earlier commit)

--- ca/strat/sper/caps/congen.f90
+++ ca/strat/sper/caps/congen.f90
@@ -216,8 +216,7 @@ if (levbeg .le. levend) then
 !$OMP& qdx,qdy,isx,isy,xcr,ycr,kib,icre,icrtab,noctab,free,xd,yd,t2Start,t2End,t2Time,t3Start,t3End,t3Time, &
 !$OMP& t4Start,t4End,t4Time,t5Start,t5End,t5Time,t6Start,t6End,t6Time,t7Start,t7End,t7Time,t8Start,t8End,t8Time, &
 !$OMP& t9Start,t9End,t9Time) &
-!$OMP& SHARED(levbeg,levend,dq,qoff,qa,ibx,xgu,ygu,glxu,glyu,ellx,hlxi,oms,ymin,ymax,nxu,nyu,nxum1,nxny,koff,f12,one, &
-!$OMP& xa,ya,inda,npa,i1a,i2a,na,npta,nextq,timing_on) &
+!$OMP& SHARED(levbeg,levend,dq,qoff,qa,ibx,xgu,ygu,xa,ya,inda,npa,i1a,i2a,na,npta,nextq,timing_on) &
 !$OMP& REDUCTION(+:l2TotTime,l3TotTime,l4TotTime,l5TotTime,l6TotTime,l7TotTime,l8TotTime,l9TotTime) DEFAULT(none)
 do lev=levbeg,levend
  !Integer index giving contour level:
